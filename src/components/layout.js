81f48365bd5acd000cef4e2d4f932daab058f8dc
Jeroen Gunster <48288969+erole-attack@users.noreply.github.com>
2021-01-12 14:47:59 +0100

    image update

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,8 @@ import "./layout.css"
 
 import Header from "./header"
 import React from "react"
-import outdatedBrowser from "outdated-browser-rework";
 
 const Layout = ({ children }) => {
-  outdatedBrowser()
   return (
     <>
       <Header>
