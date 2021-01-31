82ca581240f861d82c72ce288663d0e3a257bd94
Jeroen Gunster <48288969+erole-attack@users.noreply.github.com>
2021-02-01 00:47:18 +0100

    mobile fix

--- src/components/mobile.js
+++ src/components/mobile.js
@@ -673,7 +673,7 @@ const mobileStyles = StyleSheet.create({
   form_input: {
       width: '85vw',
       fontSize: '4.8vw',
-      textIndent: '0.5vw',
+      textIndent: '1vw',
       height: '100%',
       MozBorderRadius: '0px',
       WebkitBorderRadius: '0px',
